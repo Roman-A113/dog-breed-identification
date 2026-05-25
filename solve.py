ed9dc47b673dd7677d02362505ab2f2177544fe3
Roman <roman.koles@bk.ru>
2026-05-25 16:57:31 +0500

    efficient net v2

--- solve.py
+++ solve.py
@@ -89,11 +89,13 @@ if __name__ == "__main__":
     val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=2)
     test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=2)
 
-    model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-
-    num_features = model.fc.in_features
-    model.fc = nn.Linear(num_features, len(breed_cols))
+    model = models.efficientnet_v2_s(weights=models.EfficientNet_V2_S_Weights.DEFAULT)
+    num_features = model.classifier[1].in_features
+    model.classifier[1] = nn.Linear(num_features, len(breed_cols))
     model = model.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.AdamW(model.parameters(), lr=LEARNING_RATE)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.AdamW(model.parameters(), lr=LEARNING_RATE)
@@ -146,7 +148,7 @@ if __name__ == "__main__":
         if val_loss < best_val_loss:
             best_val_loss = val_loss
             torch.save(model.state_dict(), BEST_MODEL_PATH)
-            
+
     tqdm.write(f"Лучший loss на валидации: {best_val_loss:.5f}")
 
     model.load_state_dict(torch.load(BEST_MODEL_PATH, weights_only=True))
